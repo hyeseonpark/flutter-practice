2e2408485aab01013dd0401d01e1fd2f10ea3cec
hyeseon <hyeseon.n.p@gmail.com>
2023-01-27 18:21:57 +0900

    toonflix api

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,171 +1,25 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
-  bool isRunning = false;
-  bool ableRestart = false;
-  int totalPomodoros = 0;
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        totalPomodoros = totalPomodoros + 1;
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds = totalSeconds - 1;
-      });
-    }
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick, // 괄호를 넣으면 당장 실행한다는 뜻
-    );
-
-    setState(() {
-      isRunning = true;
-      ableRestart = false;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-
-    setState(() {
-      isRunning = false;
-      ableRestart = true;
-    });
-  }
-
-  void onRestartPressed() {
-    setState(() {
-      isRunning = false;
-      totalSeconds = twentyFiveMinutes;
-      ableRestart = false;
-    });
-    timer.cancel();
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split(".").first.substring(2);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                child: Text(format(totalSeconds),
-                    style: TextStyle(
-                      color: Theme.of(context).cardColor,
-                      fontSize: 89,
-                      fontWeight: FontWeight.w600,
-                    )),
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Center(
-                        child: IconButton(
-                          iconSize: 98,
-                          color: Theme.of(context).cardColor,
-                          onPressed: () {
-                            isRunning ? onPausePressed() : onStartPressed();
-                          },
-                          icon: Icon(isRunning
-                              ? Icons.pause_circle_outline
-                              : Icons.play_circle_outline),
-                        ),
-                      ),
-                      ableRestart
-                          ? Container(
-                              margin: const EdgeInsets.only(top: 98 + 30 + 10),
-                              child: IconButton(
-                                iconSize: 30,
-                                onPressed: () {
-                                  onRestartPressed();
-                                },
-                                icon: const Icon(
-                                  Icons.restart_alt_outlined,
-                                ),
-                              ),
-                            )
-                          : Container(),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50),
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Pomodoros',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20,
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                            ),
-                          ),
-                          Text(
-                            '$totalPomodoros',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 58,
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
   }
 }
